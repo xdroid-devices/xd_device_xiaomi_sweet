02f057d79c389c71bb0773bac3494941fe05d012
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2022-06-26 08:56:29 +0700

    sweet: Remove some Gapps packages at build time
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := remove_packages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Stk Camera2 DevicePolicyPrebuilt GoogleTTS LocationHistoryPrebuilt MarkupGoogle Photos SoundAmplifierPrebuilt talkback
-LOCAL_OVERRIDES_PACKAGES += DevicePersonalizationPrebuiltPixel2021 DMService GCS
-LOCAL_OVERRIDES_PACKAGES += GoogleRestorePrebuilt HelpRtcPrebuilt SafetyHubPrebuilt ScribePrebuilt TurboPrebuilt Velvet WellbeingPrebuilt
+LOCAL_OVERRIDES_PACKAGES := Stk Camera2
+LOCAL_OVERRIDES_PACKAGES += GoogleTTS
+LOCAL_OVERRIDES_PACKAGES += Photos
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
